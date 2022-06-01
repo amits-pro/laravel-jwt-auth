1b58ea7ff9b36847635ff6aa519ab36208e48982
Amit Saxena <amits.brg@gmail.com>
2022-06-01 17:45:25 +0530

    added validations to movie controller

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -1,13 +1,31 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
 class MovieController extends Controller
 {
     public function movies($pageId) {
+
+        $validator = Validator::make(
+            [
+              'pageId' => $pageId,
+            ],
+            [
+              'pageId' => ['required','integer'],
+            ] 
+          );
+   
+        if($validator->fails()){
+            return response()->json([
+                'success'=>false,
+                'errors'=>$validator->errors()
+            ],422);                
+        }
+
+        
 
         $token = config('services.tmdb.token');
         $uri = "https://api.themoviedb.org/3/trending/movie/week?page=".$pageId;
@@ -16,12 +34,41 @@ class MovieController extends Controller
     }
     public function movie($movieId) {
 
+        $validator = Validator::make(
+            [
+              'movieId' => $movieId,
+            ],
+            [
+              'movieId' => ['required','integer'],
+            ] 
+          );
+   
+        if($validator->fails()){
+            return response()->json([
+                'success'=>false,
+                'errors'=>$validator->errors()
+            ],422);                
+        }
+
         $token = config('services.tmdb.token');
         $uri = "https://api.themoviedb.org/3/movie/".$movieId;
         $response = Http::withToken($token)->acceptJson()->get($uri);
         return $response;
     }
     public function search(Request $request) {
+
+        $validator = Validator::make($request->all(), [
+            'page' => 'required|integer',
+            'query' => 'required|string',
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'success'=>false,
+                'errors'=>$validator->errors()
+            ],422);                
+        }
+
         $pageId = $request->input('page');
         $query = $request->input('query');
 
